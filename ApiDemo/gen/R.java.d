a173f0772cefefef81c9553eb2762e7db5d26206
Willie de Jongh <willie@trustpay.biz>
2013-06-26 13:50:07 +0200

    Simplify the file structure

--- ApiDemo/gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/Users/williedejongh/trustpaymobile/trustpay/mobile/android/ApiDemo/gen/biz/trustpay/ApiDemo/R.java \
- : /Users/williedejongh/trustpaymobile/trustpay/mobile/android/ApiDemo/res/layout/main.xml \
-/Users/williedejongh/trustpaymobile/trustpay/mobile/android/ApiDemo/res/values/strings.xml \
-/Users/williedejongh/trustpaymobile/trustpay/mobile/android/ApiDemo/bin/AndroidManifest.xml \
--- ApiDemo/gen/biz/trustpay/ApiDemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package biz.trustpay.ApiDemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
